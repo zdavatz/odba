bf29f58250cf70ee8fccbe90a64e3bc553b74de4
hwyss <hwyss@oldie.ywesee.local>
2004-11-18 07:47:20 +0100

    ChangeSet 1.27: fix disappearing name problem sql/update_object.sql: fix disappearing name problem

--- sql/update_object.sql
+++ sql/update_object.sql
@@ -1,10 +1,18 @@
 CREATE OR REPLACE FUNCTION update_object(integer, text, text, boolean) 
 RETURNS BOOLEAN AS '
+DECLARE 
+	v_old_name text;
+	v_new_name text;
 BEGIN
-	PERFORM odba_id FROM object WHERE odba_id = $1;
+	SELECT INTO v_old_name name FROM object WHERE odba_id = $1;
 	IF FOUND THEN
+		IF $3 IS NULL THEN
+			v_new_name := v_old_name;
+		ELSE
+			v_new_name := $3;
+		END IF;
 		UPDATE object 
-		SET content = $2, name = $3, prefetchable = $4
+		SET content = $2, name = v_new_name, prefetchable = $4
 		WHERE odba_id = $1;
 	ELSE
 		INSERT INTO object (odba_id, content, name, prefetchable)
