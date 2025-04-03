zdavatz/odba

commit 92f857237a001a91f3e991b29a6629c43603c402

Drop text search dictionaries/configuration before recreating. Fix fulltext search

lib/odba/storage.rb
@@ -158,11 +158,14 @@ CREATE INDEX IF NOT EXISTS target_id_#{table_name} ON #{table_name}(target_id);
     end
     def create_fulltext_index(table_name)
       self.dbi.do <<-SQL
-CREATE TABLE IF NOT EXISTS #{table_name} (
+DROP TABLE IF EXISTS #{table_name};
+      SQL
+      self.dbi.do <<-SQL
+CREATE TABLE IF NOT EXISTS #{table_name}  (
   origin_id INTEGER,
   search_term tsvector,
   target_id INTEGER
-);
+) WITH OIDS ;
       SQL
       #index origin_id
       self.dbi.do <<-SQL
@@ -179,11 +182,14 @@ CREATE INDEX IF NOT EXISTS target_id_#{table_name} ON #{table_name}(target_id);
     end
     def create_index(table_name)
       self.dbi.do <<-SQL
+        DROP TABLE IF EXISTS #{table_name};
+      SQL
+      self.dbi.do <<-SQL
         CREATE TABLE IF NOT EXISTS #{table_name} (
           origin_id INTEGER,
           search_term TEXT,
           target_id INTEGER
-        );
+        )  WITH OIDS;
       SQL
       #index origin_id
       self.dbi.do <<-SQL
@@ -205,7 +211,7 @@ CREATE INDEX IF NOT EXISTS target_id_#{table_name} ON #{table_name}(target_id);
 			Thread.current[:txn] || @dbi
 		end
 		def drop_index(index_name)
-			self.dbi.do "DROP TABLE #{index_name}"
+			self.dbi.do "DROP TABLE IF EXISTS #{index_name}"
 		end
     def delete_index_element(index_name, odba_id, id_name)
       self.dbi.do <<-SQL, odba_id
@@ -301,13 +307,20 @@ CREATE INDEX IF NOT EXISTS target_id_#{table_name} ON #{table_name}(target_id);
     def generate_dictionary(language)
       # postgres searches for the dictionary file in the directory share/tsearch_data of it installation location
       # By default under gentoo, this is /usr/share/postgresql/tsearch_data/
+      # Use /usr/local/pgsql-10.1/bin/pg_config --sharedir to get the current value
       # As we have no way to get the current installation path, we do not check whether the files are present or not
       file='fulltext'
       # setup configuration
       self.dbi.do <<-SQL
+        DROP TEXT SEARCH DICTIONARY IF EXISTS  public.default_#{language};
+      SQL
+      self.dbi.do <<-SQL
         CREATE TEXT SEARCH CONFIGURATION public.default_#{language} ( COPY = pg_catalog.#{language} );
       SQL
       # ispell
+      self.dbi.do <<-SQL
+        DROP TEXT SEARCH DICTIONARY IF EXISTS  #{language}_ispell;
+      SQL
       self.dbi.do <<-SQL
         CREATE TEXT SEARCH DICTIONARY #{language}_ispell (
           TEMPLATE  = ispell,
@@ -460,7 +473,7 @@ CREATE INDEX IF NOT EXISTS target_id_#{table_name} ON #{table_name}(target_id);
       end
       self.dbi.select_all(sql, *values)
     end
-		def retrieve_from_fulltext_index(index_name, search_term, dict, limit=nil)
+		def retrieve_from_fulltext_index(index_name, search_term, limit=nil)
       ## this combination of gsub statements solves the problem of 
       #  properly escaping strings of this form: "(2:1)" into 
       #  '\(2\:1\)' (see test_retrieve_from_fulltext_index)
@@ -468,19 +481,19 @@ CREATE INDEX IF NOT EXISTS target_id_#{table_name} ON #{table_name}(target_id);
         .gsub(/[():]/i, '\\ \\&').gsub(/\s/, '')
 	    sql = <<-EOQ
 				SELECT target_id, 
-					max(ts_rank(search_term, to_tsquery(?, ?))) AS relevance
+					max(ts_rank(search_term, to_tsquery(?))) AS relevance
 				FROM #{index_name} 
-				WHERE search_term @@ to_tsquery(?, ?) 
+				WHERE search_term @@ to_tsquery(?)
 				GROUP BY target_id
 				ORDER BY relevance DESC
 			EOQ
       if(limit)
         sql << " LIMIT #{limit}"
       end
-			self.dbi.select_all(sql, dict, term, dict, term)
+			self.dbi.select_all(sql, term, term)
 		rescue DBI::ProgrammingError => e
 			warn("ODBA::Storage.retrieve_from_fulltext_index rescued a DBI::ProgrammingError(#{e.message}). Query:")
-			warn("self.dbi.select_all(#{sql}, #{dict}, #{term}, #{dict}, #{term})")
+			warn("self.dbi.select_all(#{sql}, #{term}, #{term})")
 			warn("returning empty result")
 			[]
 		end
@@ -583,17 +596,21 @@ WHERE origin_id = ?
     def update_fulltext_index(index_name, origin_id, search_term, target_id, dict)
       search_term = search_term.gsub(/\s+/, ' ').strip
       if(target_id)
-        self.dbi.do <<-SQL, origin_id, dict, search_term, target_id
+                         value = <<-SQL, origin_id.to_s, search_term, target_id
 INSERT INTO #{index_name} (origin_id, search_term, target_id)
-VALUES (?, to_tsvector(?, ?), ?)
+VALUES (?, to_tsvector(?), ?)
+        SQL
+        result = self.dbi.do <<-SQL, origin_id.to_s, search_term, target_id
+INSERT INTO #{index_name} (origin_id, search_term, target_id)
+VALUES (?, to_tsvector(?), ?)
         SQL
       else
-        self.dbi.do <<-SQL, dict, search_term, origin_id
-UPDATE #{index_name} SET search_term=to_tsvector(?, ?)
+        result = self.dbi.do <<-SQL, search_term, origin_id
+UPDATE #{index_name} SET search_term=to_tsvector(?)
 WHERE origin_id=?
         SQL
       end
-    end
+      end
 		def update_index(index_name, origin_id, search_term, target_id)
       if(target_id)
         self.dbi.do <<-SQL, origin_id, search_term, target_id

lib/odba/index.rb
@@ -23,7 +23,7 @@ module ODBA
       ]
     end
 		attr_accessor :origin_klass, :target_klass, :resolve_origin, :resolve_target,
-			:resolve_search_term, :index_name, :dictionary, :class_filter
+			:resolve_search_term, :index_name, :class_filter
 		def initialize(index_definition, origin_module)
 			@origin_klass = origin_module.instance_eval(index_definition.origin_klass.to_s)
 			@target_klass = origin_module.instance_eval(index_definition.target_klass.to_s)
@@ -31,7 +31,6 @@ module ODBA
 			@resolve_target = index_definition.resolve_target
 			@index_name = index_definition.index_name
 			@resolve_search_term = index_definition.resolve_search_term
-			@dictionary = index_definition.dictionary
       @class_filter = index_definition.class_filter
 		end
     def current_origin_ids(target_id) # :nodoc:
@@ -381,15 +380,12 @@ module ODBA
     end
 		def fetch_ids(search_term, meta=nil)  # :nodoc:
       limit = meta.respond_to?(:limit) && meta.limit
-			rows = ODBA.storage.retrieve_from_fulltext_index(@index_name, 
-				search_term, @dictionary, limit)
+			rows = ODBA.storage.retrieve_from_fulltext_index(@index_name, search_term, limit)
 			set_relevance(meta, rows)
 			rows.collect { |row| row.at(0) }
 		end
     def do_update_index(origin_id, search_text, target_id=nil) # :nodoc:
-      ODBA.storage.update_fulltext_index(@index_name, origin_id,
-                                         search_text, target_id, 
-                                         @dictionary)
+      ODBA.storage.update_fulltext_index(@index_name, origin_id, search_text, target_id)
     end
   end
 end

lib/odba/index_definition.rb
@@ -5,8 +5,8 @@ module ODBA
 	# IndexDefinition is a convenience class. Load a yaml-dump of this and pass it
 	# to Cache#create_index to introduce new indices 
 	class IndexDefinition
-		attr_accessor :index_name, :dictionary, :origin_klass, 
-			:target_klass, :resolve_search_term, :resolve_target, 
+		attr_accessor :index_name, :origin_klass,
+			:target_klass, :resolve_search_term, :resolve_target,
 			:resolve_origin, :fulltext, :init_source, :class_filter
 		def initialize
 			@index_name = ""
@@ -15,7 +15,6 @@ module ODBA
 			@resolve_search_term = ""
 			@resolve_target = ""
 			@resolve_origin = ""
-			@dictionary = ""
 			@init_source = ""
 			@fulltext = false
       @class_filter = :is_a?

commit ca146fb4dee31c963e500e34e963ee260b709945

Remove WITH OIDS when creating tables. These were wrongly introduced with commit 92f857237a001a91f3e991b29a6629c43603c402 in 2017. They are not needed and are forbidden with PostreSQL 12 or higher

lib/odba/storage.rb
@@ -173,7 +173,7 @@ module ODBA
            origin_id INTEGER,
            search_term tsvector,
            target_id INTEGER
-         ) WITH OIDS ;
+         ) ;
       SQL
       # index origin_id
       dbi.do <<~SQL
@@ -198,7 +198,7 @@ module ODBA
           origin_id INTEGER,
           search_term TEXT,
           target_id INTEGER
-        )  WITH OIDS;
+        ) ;
       SQL
       # index origin_id
       dbi.do <<-SQL

test/test_storage.rb
@@ -101,7 +101,7 @@ module ODBA
       dbi.should_receive(:do).once.with(sql)
       sql = "        CREATE INDEX IF NOT EXISTS target_id_index_name\n        ON index_name(target_id)\n"
       dbi.should_receive(:do).once.with(sql)
-      sql = "        CREATE TABLE IF NOT EXISTS index_name (\n          origin_id INTEGER,\n          search_term TEXT,\n          target_id INTEGER\n        )  WITH OIDS;\n"
+      sql = "        CREATE TABLE IF NOT EXISTS index_name (\n          origin_id INTEGER,\n          search_term TEXT,\n          target_id INTEGER\n        ) ;\n"
       dbi.should_receive(:do).once.with(sql)
 			@storage.create_index("index_name")
 		end
@@ -461,7 +461,7 @@ CREATE INDEX IF NOT EXISTS target_id_conditions ON conditions(target_id);
       @dbi.should_receive(:do).once.with(sql).and_return
       sql = "CREATE INDEX IF NOT EXISTS target_id_fulltext ON fulltext(target_id);\n"
       @dbi.should_receive(:do).once.with(sql).and_return
-      sql = "CREATE TABLE IF NOT EXISTS fulltext  (\n  origin_id INTEGER,\n  search_term tsvector,\n  target_id INTEGER\n) WITH OIDS ;\n"
+      sql = "CREATE TABLE IF NOT EXISTS fulltext  (\n  origin_id INTEGER,\n  search_term tsvector,\n  target_id INTEGER\n) ;\n"
       @dbi.should_receive(:do).once.with(sql).and_return
       sql = "CREATE INDEX IF NOT EXISTS search_term_fulltext\nON fulltext USING gist(search_term);\n"
       @dbi.should_receive(:do).once.with(sql).and_return