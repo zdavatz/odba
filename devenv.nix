dfdaf5f085ffeb8acab8160933ada17f2741c45a
Niklaus Giger <niklaus.giger@member.fsf.org>
2025-04-04 18:38:39 +0200

    Added running test/example using devenv in a github action

---
+++ devenv.nix
@@ -0,0 +1,50 @@
+{
+  inputs,
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git pkgs.libyaml ];
+
+  enterShell = ''
+    echo This is the devenv shell for odba_test
+    git --version
+    ruby --version
+    psql --version
+    bundle install
+  '';
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_16;
+#    package = pkgs-old.postgresql_10;
+    listen_addresses = "0.0.0.0";
+    port = 5432;
+
+    initialDatabases = [
+      { name = "odba_test"; }
+    ];
+
+    initdbArgs =
+      [
+        "--locale=C"
+        "--encoding=UTF8"
+      ];
+
+    initialScript = ''
+      create role odba_test superuser login password null;
+    '';
+  };
+  languages.ruby.enable = true;
+  languages.ruby.version = "3.3";
+  # See full reference at https://devenv.sh/reference/options/
+  enterTest = ''
+    devenv up --detach
+    bundle exec test/example.rb
+  '';
+}
