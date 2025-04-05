9e21cf33a4484713ae76ed404d454c925912d891
Niklaus Giger <niklaus.giger@member.fsf.org>
2025-04-05 09:59:20 +0200

    Ran standardrb --fix for test

--- test/test_id_server.rb
+++ test/test_id_server.rb
@@ -2,44 +2,48 @@
 # TestIdServer -- odba -- 10.11.2004 -- hwyss@ywesee.com
 
 $: << File.dirname(__FILE__)
-$: << File.expand_path('../lib', File.dirname(__FILE__))
+$: << File.expand_path("../lib", File.dirname(__FILE__))
 
-require 'simplecov'
+require "simplecov"
 require "test/unit"
 require "flexmock/test_unit"
-require 'odba/id_server'
-require 'odba/odba'
-require 'odba/marshal'
+require "odba/id_server"
+require "odba/odba"
+require "odba/marshal"
 
 module ODBA
-	class TestIdServer < Test::Unit::TestCase
+  class TestIdServer < Test::Unit::TestCase
     include FlexMock::TestCase
-		def setup
-			@cache = ODBA.cache = flexmock('cache')
-			@id_server = IdServer.new
-			@id_server.instance_variable_set('@odba_id', 1)
-		end
+    def setup
+      @cache = ODBA.cache = flexmock("cache")
+      @id_server = IdServer.new
+      @id_server.instance_variable_set(:@odba_id, 1)
+    end
+
     def teardown
       @id_server = nil
       super
     end
-		def test_first
+
+    def test_first
       @cache.should_receive(:store).with(@id_server).times(3)
-			assert_equal(1, @id_server.next_id(:foo))
-			assert_equal(1, @id_server.next_id(:bar))
-			assert_equal(1, @id_server.next_id(:baz))
-		end
-		def test_consecutive
+      assert_equal(1, @id_server.next_id(:foo))
+      assert_equal(1, @id_server.next_id(:bar))
+      assert_equal(1, @id_server.next_id(:baz))
+    end
+
+    def test_consecutive
       @cache.should_receive(:store).with(@id_server).times(3)
-			assert_equal(1, @id_server.next_id(:foo))
-			assert_equal(2, @id_server.next_id(:foo))
-			assert_equal(3, @id_server.next_id(:foo))
-		end
-		def test_dumpable
+      assert_equal(1, @id_server.next_id(:foo))
+      assert_equal(2, @id_server.next_id(:foo))
+      assert_equal(3, @id_server.next_id(:foo))
+    end
+
+    def test_dumpable
       @cache.should_receive(:store).with(@id_server).times(1)
-			@id_server.next_id(:foo)
-			dump = @id_server.odba_isolated_dump 
-			assert_instance_of(ODBA::IdServer, ODBA.marshaller.load(dump))
-		end
-	end
+      @id_server.next_id(:foo)
+      dump = @id_server.odba_isolated_dump
+      assert_instance_of(ODBA::IdServer, ODBA.marshaller.load(dump))
+    end
+  end
 end
